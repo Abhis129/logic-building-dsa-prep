2799d3d0400593febc5c998a8b5c3d9621697479
Abhishek Pandey <abhipandey8761@gmail.com>
2026-03-19 02:25:21 +0530

    Completed Phase 2 Level 2 Basics Looping

--- Phase_2.py/level_1.py
+++ Phase_2.py/level_1.py
@@ -37,3 +37,52 @@ n = int(input("Enter a number: "))
 for i in range(1, 11):
     print(f"{n} x {i} = {n * i}")
 
+# 6. Print the sum of first n natural numbers
+print("\n6. Sum of first n natural numbers:")
+n = int(input("Enter n: "))
+sum_n = 0
+for i in range(1, n + 1):
+    sum_n += i
+print("Sum =", sum_n)
+
+
+# 7. Print the sum of all even numbers up to n
+print("\n7. Sum of even numbers up to n:")
+n = int(input("Enter n: "))
+even_sum = 0
+for i in range(1, n + 1):
+    if i % 2 == 0:
+        even_sum += i
+print("Even Sum =", even_sum)
+
+
+# 8. Print the sum of all odd numbers up to n
+print("\n8. Sum of odd numbers up to n:")
+n = int(input("Enter n: "))
+odd_sum = 0
+for i in range(1, n + 1):
+    if i % 2 != 0:
+        odd_sum += i
+print("Odd Sum =", odd_sum)
+
+
+# 9. Print the factorial of a given number
+print("\n9. Factorial of a number:")
+n = int(input("Enter a number: "))
+fact = 1
+for i in range(1, n + 1):
+    fact *= i
+print("Factorial =", fact)
+
+
+# 10. Print the product of digits of a given number
+print("\n10. Product of digits:")
+num = int(input("Enter a number: "))
+product = 1
+
+while num > 0:
+    digit = num % 10
+    product *= digit
+    num //= 10
+
+print("Product of digits =", product)
